5ca1b0843568b39ffed08bd9c61660a9131a56a6
leadpogrommer <ilyamerzlakov@gmail.com>
2023-11-18 12:59:07 +0700

    Better map operation logging

--- distributed_map.py
+++ distributed_map.py
@@ -17,8 +17,11 @@ class DistributedDict(dict):
     def apply_log_entry(self, entry):
         match entry:
             case MapSet():
+                print(f'Applying set operation: {entry}')
                 self[entry.key] = entry.value
             case MapRemove():
+                print(f'Applying del operation: {entry}')
                 del self[entry.key]
             case _:
                 print(f'Trying to apply unknown action: {entry}')
+        print(f'Map state: {self}')
